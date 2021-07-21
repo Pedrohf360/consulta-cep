515c644f762e4899091de675379cb82335f35f3c
PedroH <pedro.henriqueff10@gmail.com>
2021-07-21 12:21:11 -0300

    add swagger module for API documentation

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
 import { config } from 'dotenv';
+config();
+
 import routes from './routes';
 import express from 'express';
 import { createServer, Server as HttpServer } from 'http';
 import { env } from './config/globals';
+const swaggerDocument = require('./swagger.json');
+import swaggerUi from 'swagger-ui-express';
 
-config();
+const port = env.PORT;
 
 const app: express.Application = express();
 const server: HttpServer = createServer(app);
+
+if (env.ENV === 'development') {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-const port = env.PORT;
-
 server.listen(port, () => {
-    console.log('##################################');
-    console.log(`Listening at http://localhost:${port}/`);
-    console.log('##################################');
+    console.log(`API listening at http://localhost:${port}/`);
+    console.log(`Swagger listening at http://localhost:${port}/api-docs`);
 });
