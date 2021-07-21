dd30f82b13e4454f9a4d60aae1c7d297875e12a8
PedroH <pedro.henriqueff10@gmail.com>
2021-07-21 02:35:49 -0300

    set folders structure changes

---
+++ src/config/globals.ts
@@ -0,0 +1,5 @@
+// Environment variables imported from .env file
+export const env = {
+    PORT: process.env.PORT || 3000,
+    ENV: process.env.ENV
+}
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { config } from 'dotenv';
+import routes from './routes';
+import express from 'express';
+import { createServer, Server as HttpServer } from 'http';
+import { env } from './config/globals';
+
+config();
+
+const app: express.Application = express();
+const server: HttpServer = createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+
+const port = env.PORT;
+
+server.listen(port, () => {
+    console.log(`Live at http://localhost:${port}/`);
+});
